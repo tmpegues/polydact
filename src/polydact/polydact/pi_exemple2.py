f9651cc143b8909e491aa64672f3520e2f6bc4b4
Miguel Pegues <MiguelP@u.northwestern.edu>
2026-03-08 13:39:03 -0500

    venv removal

--- src/polydact/polydact/pi_exemple2.py
+++ src/polydact/polydact/pi_exemple2.py
@@ -5,6 +5,6 @@ adc = [MCP3008(channel=i) for i in range(8)]
 
 while True:
     for channel in [1, 2, 3]:
-        print(channel.value)
+        print(f'{channel}: {adc[channel].value}')
 
     time.sleep(0.1)
