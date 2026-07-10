27e6eb6409bc6db863bc67bfab0491aa40a819d2
Miguel Pegues <MiguelP@u.northwestern.edu>
2026-07-09 23:36:48 -0700

    Got it moving alright. Need more current, I think

--- pico_slide_pot/pico_slide_pot.c
+++ pico_slide_pot/pico_slide_pot.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 #include "hardware/pwm.h"
@@ -18,27 +19,63 @@ int ENABLE[3] = {13, 16, 19};
 int PIN_A[3] = {14, 17, 20};
 int PIN_B[3] = {15, 18, 21};
 
-void set_motor(int motor, int duty)
+float read_pot(int i)
 {
-    duty *= 100;
-    if (duty > 1000)
+    adc_select_input(POT[i]);
+    float value = (float)adc_read();
+    //    value = adc_read();
+    value -= 4095 / 2;
+    value *= 100;
+    value /= 4095 / 2;
+
+    return value;
+}
+
+void set_motor_pos(int motor, float goal)
+{
+
+    int duty = (read_pot(motor) - goal) * 100;
+    if (duty > 0)
     {
 
         gpio_put(PIN_A[motor], 1);
         gpio_put(PIN_B[motor], 0);
         pwm_set_gpio_level(ENABLE[motor], duty);
     }
-    else if (duty < -1000)
+    else if (duty < 0)
     {
 
         gpio_put(PIN_A[motor], 0);
         gpio_put(PIN_B[motor], 1);
         pwm_set_gpio_level(ENABLE[motor], -duty);
-
     }
     else
     {
-    pwm_set_gpio_level(ENABLE[motor], 0);
+        pwm_set_gpio_level(ENABLE[motor], 0);
+    }
+}
+
+void set_motor_speed(int motor, float speed)
+{
+
+    int duty = (int)speed;
+    if (duty > 0)
+    {
+
+        gpio_put(PIN_A[motor], 1);
+        gpio_put(PIN_B[motor], 0);
+        pwm_set_gpio_level(ENABLE[motor], duty);
+    }
+    else if (duty < 0)
+    {
+
+        gpio_put(PIN_A[motor], 0);
+        gpio_put(PIN_B[motor], 1);
+        pwm_set_gpio_level(ENABLE[motor], -duty);
+    }
+    else
+    {
+        pwm_set_gpio_level(ENABLE[motor], 0);
     }
 }
 
@@ -61,32 +98,30 @@ void init_motors(int num_motors)
     }
 }
 
-int read_pot(int i)
-{
-    adc_select_input(POT[i]);
-    int value = 0;
-    value = adc_read();
-    value -= 4095 / 2;
-    value *= 100;
-    value /= 4095 / 2;
-
-    return value;
-}
-
 int main()
 {
-    await_usb(0, true);
+    await_usb(1, true);
     init_motors(3);
     adc_init();
 
-    int values[3];
+    float step = 900;
+    float change = .1;
     while (true)
     {
+        step += change;
+        // if (step > 50 || step < -50)
+        // {
+        //     change *= -1;
+        // }
+        float goal = 0;
+        (float)sin(step) * 100;
         for (int i = 0; i < 3; i++)
         {
-            values[i] = read_pot(POT[i]);
-            set_motor(i, values[i]);
+            goal = (float)sin(step+i*3.14*2/3) * 100;
+            printf("%f\n", goal);
+            set_motor_pos(i, goal);
         }
-        printf("%d %d %d\n ", values[0], values[1], values[2]);
+        printf("\n");
+        sleep_ms(10);
     }
 }
