86a92dcd215b924d6fe32e00c00ed966c00afe20
Miguel Pegues <MiguelP@u.northwestern.edu>
2026-07-11 20:31:03 -0700

    Got rid of vibration. Now chaning power supply.

--- pico_slide_pot/pico_slide_pot.c
+++ pico_slide_pot/pico_slide_pot.c
@@ -15,11 +15,18 @@
 #define WRAP 1000
 
 #define SUPPLY_V 6.0
+#define NUM_MOTORS 3
 #define MIN_V 5.0
 #define MAX_V 10.0
 
 #define MIN_DUTY MIN_V / SUPPLY_V *WRAP
 #define MAX_DUTY SUPPLY_V > MAX_V ? (MAX_V / SUPPLY_V * WRAP) : WRAP
+
+#define P 0.5
+#define I 0.0
+
+struct repeating_timer timer1;
+float goal[3] = {4095 / 2, 4095 / 2, 4095 / 2};
 
 int POT[3] = {0, 1, 2};
 int ENABLE[3] = {13, 16, 19};
@@ -33,37 +40,54 @@ int read_pot(int i)
     return adc_read();
 }
 
-void set_motor_pos(int motor, int goal)
+void position_pid(int motor, int desired)
 {
     int pos = read_pot(motor);
-    float duty = ((pos - goal) / 4095.0);
+    int error = pos - desired;
+    static float integral;
+    static int last_pos[NUM_MOTORS];
 
-    printf("motor cur goal duty %d %4d %4d %.4f\n", motor, pos, goal, duty);
-
-    if (duty > 0)
+    if (error < 50 && error > -50)
     {
-        duty = duty  * (MAX_DUTY - MIN_DUTY) + MIN_DUTY;
-        gpio_put(PIN_A[motor], 1);
-        gpio_put(PIN_B[motor], 0);
-        pwm_set_gpio_level(ENABLE[motor], duty);
-    }
-    else if (duty < 0)
-    {
-        duty = -duty * (MAX_DUTY - MIN_DUTY) + MIN_DUTY;
-        gpio_put(PIN_A[motor], 0);
-        gpio_put(PIN_B[motor], 1);
-        pwm_set_gpio_level(ENABLE[motor], duty);
+        integral = 0;
+        pwm_set_gpio_level(ENABLE[motor], 0);
+        // printf("motor cur goal error duty %d %4d %4d %4d %4d *\n", motor, pos, desired, error, desired);
     }
     else
     {
-        pwm_set_gpio_level(ENABLE[motor], 0);
+        integral += error;
+        integral = integral > 4095 / 2 ? 4095 / 2 : integral;
+
+        float duty = P * error + I * integral;
+
+        // printf("motor cur goal error duty %d %4d %4d %4d %4d %.4f\n", motor, pos, desired, error, desired, duty);
+
+        if (duty > 0)
+        {
+            duty = duty * (MAX_DUTY - MIN_DUTY) + MIN_DUTY;
+            gpio_put(PIN_A[motor], 1);
+            gpio_put(PIN_B[motor], 0);
+            pwm_set_gpio_level(ENABLE[motor], duty);
+        }
+        else if (duty < 0)
+        {
+            duty = -duty * (MAX_DUTY - MIN_DUTY) + MIN_DUTY;
+            gpio_put(PIN_A[motor], 0);
+            gpio_put(PIN_B[motor], 1);
+            pwm_set_gpio_level(ENABLE[motor], duty);
+        }
+        else
+        {
+            // pwm_set_gpio_level(ENABLE[motor], 0);
+        }
+        // printf("%f\n", duty);
     }
-    printf("%f\n", duty);
+    last_pos[motor] = pos;
 }
 
-void init_motors(int num_motors)
+void init_motors()
 {
-    for (int i = 0; i < num_motors; i++)
+    for (int i = 0; i < NUM_MOTORS; i++)
     {
         gpio_init(PIN_A[i]);
         gpio_init(PIN_B[i]);
@@ -80,20 +104,36 @@ void init_motors(int num_motors)
     }
 }
 
+bool pid_callback(__unused struct repeating_timer *t)
+{
+    for (int i = 0; i < NUM_MOTORS; i++)
+    {
+        position_pid(i, goal[i]);
+    }
+    // printf("\n");
+    return true;
+}
+
 int main()
 {
     await_usb(1, true);
-    init_motors(3);
+    init_motors();
     adc_init();
+    add_repeating_timer_ms(1, pid_callback, NULL, &timer1);
+    int step = 0;
+    float change = .01;
 
     while (true)
     {
-        for (int i = 0; i < 3; i++)
+        step++;
+        if (step % 10 == 0)
         {
-            // set_motor_pos(i, (int)((1.0 - i / 3.0) * read_pot(0)));
-            set_motor_pos(i, 0);
+            for (int i = 0; i < NUM_MOTORS; i++)
+            {
+                goal[i] = sin(step * change + i * (2 * 3.14) / 3.0) * 1800 + 4095 / 2;
+            }
         }
-        printf("\n");
-        sleep_ms(10);
+        sleep_ms(1);
+
     }
 }
