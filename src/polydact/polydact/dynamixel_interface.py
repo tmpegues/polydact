674799748b35c57c13dd854663f52aa172ef5d28
Eric <eric.rosenfeld@productiverobotics.com>
2026-07-15 09:23:19 -0700

    Set baudrate to 1 Mbps to match motor config

--- src/polydact/polydact/dynamixel_interface.py
+++ src/polydact/polydact/dynamixel_interface.py
@@ -22,7 +22,7 @@ ADDR_PRESENT_VELOCITY = 128
 PROTOCOL_VERSION = 2.0  # Default Protocol version of DYNAMIXEL X series.
 
 # Default settings
-BAUDRATE = 57600  # Dynamixel default baudrate : 57600
+BAUDRATE = 1000000  # Motors are configured for 1 Mbps
 DEVICE_NAME = '/dev/ttyUSB0'  # Check which port is being used on your controller
 
 TORQUE_ENABLE = 1  # Value for enabling the torque
